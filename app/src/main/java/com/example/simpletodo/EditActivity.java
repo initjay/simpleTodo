cfb5f670d3000b25491834722ca6013dfa8dcbe7
James Talavera <jtalavera@fb.com>
2020-06-23 17:21:15 -0700

    Created new intent to store edits and implemented returning window after save is pushed.

--- app/src/main/java/com/example/simpletodo/EditActivity.java
+++ app/src/main/java/com/example/simpletodo/EditActivity.java
@@ -2,7 +2,9 @@ package com.example.simpletodo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -22,5 +24,23 @@ public class EditActivity extends AppCompatActivity {
         // change title
         getSupportActionBar().setTitle("Edit Item");
 
+        // to populate edit text bar
+        etItem.setText(getIntent().getStringExtra(MainActivity.KEY_ITEM_TEXT));
+
+        // When the user is done editing, click on save button
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // create an intent which will contain the results
+                Intent intent = new Intent();
+                // pass the data (results of edit)
+                intent.putExtra(MainActivity.KEY_ITEM_TEXT, etItem.getText().toString());
+                intent.putExtra(MainActivity.KEY_ITEM_POSITION, getIntent().getExtras().getInt(MainActivity.KEY_ITEM_POSITION));
+                // set the result of the intent
+                setResult(RESULT_OK, intent);
+                // finish the activity (close the screen and go back)
+                finish();
+            }
+        });
     }
 }
